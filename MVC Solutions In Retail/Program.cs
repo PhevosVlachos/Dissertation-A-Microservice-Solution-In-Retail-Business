c78d7e5e9ea21daabff9f90ed89d489eb2f466c9
phevosVlachos <foivvlac@gmail.com>
2022-11-23 22:44:36 +0200

    Succesfully migrated using Pomelo database provider

--- MVC Solutions In Retail/Program.cs
+++ MVC Solutions In Retail/Program.cs
@@ -4,6 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using MVC_Solutions_In_Retail.Data;
 using MVC_Solutions_In_Retail.Services;
 
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,8 +18,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<MyDbContext>(options =>
-        options.UseMySQL(builder.Configuration.GetConnectionString("MyConn")));
+
+var connectionString = builder.Configuration.GetConnectionString("MyConn");
+
+builder.Services.AddDbContext<MyDbContext>(options => 
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 builder.Services.AddScoped<IForecast, Forecast>();
 var app = builder.Build();
 
@@ -34,5 +40,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
 
 //commentootoot
